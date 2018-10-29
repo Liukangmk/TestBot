b0263c6cabf1a4e055c01637ca09ab7911574936
VasanthManiVasi <44256983+VasanthManiVasi@users.noreply.github.com>
2018-10-29 22:21:53 +0530

    Create source.js

---
+++ commands/source.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'source',
+	description: 'sends the source',
+	execute(message, args) {
+		message.channel.send(`My author is LiuKang001#3128. Take your time to star my repo: https://github.com/Liukangmk/TestBot/`);
+	},
+};
